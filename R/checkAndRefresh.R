314b0c225e936e7dcaec01594469c50f2736fc33
Fabian Schär <fabi.schaer@unibas.ch>
2018-08-13 11:13:34 +0200

    Add checkAndRefresh and update Plot function
    * Plot now uses checkAndRefresh() to automatically obtain the newest data.
    * Auto check if Sys.Date is equal to last data entry point.

---
+++ R/checkAndRefresh.R
@@ -0,0 +1,22 @@
+##############################
+# Check and Refresh
+##############################
+
+checkAndRefresh <- function(coin, store = TRUE){
+  if(exists(coin) == FALSE){
+    coinTemp <- fromJSON(paste("http://www.coincap.io/history/",coin,sep=""))
+    if(store == TRUE){
+      write_json(coinTemp,path = paste(coin,".json",sep=""))
+    }
+    prepareCoinData(coin)
+    return(coinTemp)
+  }else if(Sys.Date() != as.Date(as.POSIXct(as.numeric( eval(parse(text= eval(parse(text = "coin"))))$price[nrow(eval(parse(text= eval(parse(text = "coin"))))$price),1])/1000, origin="1970-01-01",tz="GMT"))){
+    coinTemp <- fromJSON(paste("http://www.coincap.io/history/",coin,sep=""))
+    prepareCoinData(coin)
+    if(store == TRUE){
+      write_json(coinTemp,path = paste(coin,".json",sep=""))
+    }
+  }else{
+    return(get(coin))
+  }
+}
